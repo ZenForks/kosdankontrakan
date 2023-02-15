641f567c8c05f34e1f2027dc855bfb6fe54b011c
arofin <mohdavidarofin@gmail.com>
2023-02-15 20:53:48 +0700

    commit message

---
+++ src/pages/Notfound.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export class Notfound extends Component {
+  render() {
+    return (
+      <h1 style={{textAlign: 'center'}}>404 Not Found</h1>
+    )
+  }
+}
+export default Notfound
